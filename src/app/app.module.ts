f3d3d398051eaf3f72315f151fd7bf3b2af28c74
janicaperez <janicavictoria.perez@gmail.com>
2023-03-17 10:54:02 +0800

    Completed the project

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -11,6 +11,12 @@ import { ProductTracklistingComponent } from './product-tracklisting/product-tra
 import { ProductListComponent } from './product-list/product-list.component';
 import { RouterModule, Routes } from '@angular/router';
 
+const appRoutes: Routes = [
+  { path: 'products', component: ProductListComponent },
+  { path: 'product/:id', component: ProductPageComponent },
+  { path: '', redirectTo: 'products', pathMatch: 'full' }
+]
+
 @NgModule({
   declarations: [
     AppComponent,
@@ -22,7 +28,8 @@ import { RouterModule, Routes } from '@angular/router';
   imports: [
     BrowserModule,
     FormsModule,
-    HttpModule
+    HttpModule,
+    RouterModule.forRoot(appRoutes)
   ],
   providers: [ProductService],
   bootstrap: [AppComponent]
